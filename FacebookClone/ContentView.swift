de87f3162fcb98b75c8b6c30a2aae409144093c0
Naman Singh <naman1897@icloud.com>
2021-11-10 23:58:38 +0530

    Initial Commit

---
+++ FacebookClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacebookClone
+//
+//  Created by Naman Singh on 10/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FacebookClone/FacebookCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookCloneApp.swift
+//  FacebookClone
+//
+//  Created by Naman Singh on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FacebookCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
